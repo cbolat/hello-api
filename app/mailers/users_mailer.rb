610e78283b98ef63e6ccb02be213b1fe088ce9ad
Vili Grigore <vili.grigore@unifiedpost.com>
2019-06-26 14:28:36 +0300

    Please unban me, Mr. MailGun

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -3,8 +3,7 @@ class UsersMailer < ActionMailer::Base
   def welcome_email(email)
 
     @email = email
-    RestClient.post "https://api:key-32671ce9b0a7a29c51aa9d8466cd40b1"\
-	                  "@api.mailgun.net/v3/sandbox81f573c040064bdaad555c6175c7beb7.mailgun.org/messages",
+
 
                     :from => "Excited User <mailgun@sandbox81f573c040064bdaad555c6175c7beb7.mailgun.org>",
                     :to => @email,
