6981f868a4e3c95d73cabe32b4144c99d654397f
Vili Grigore <vili.grigore@unifiedpost.com>
2019-06-28 13:27:10 +0300

    Heroku training

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -3,12 +3,11 @@ class UsersMailer < ActionMailer::Base
   def welcome_email(email)
 
     @email = email
-
-
+    mail(
                     :from => "Excited User <mailgun@sandbox81f573c040064bdaad555c6175c7beb7.mailgun.org>",
                     :to => @email,
                     :subject => "Hello",
                     :text => "Testing some Mailgun awesomness!"
-
+    )
     end
   end
