63eee2c08123ac5af08fe6976c39ebff73091730
Vili Grigore <vili.grigore@unifiedpost.com>
2019-06-28 16:03:03 +0300

    Fixed

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -5,11 +5,11 @@ class JsonWebToken
   def self.encode(payload, exp = 24.hours.from_now)
 
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, 'john e boss')
   end
 
   def self.decode(token)
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, 'john e boss')[0]
     HashWithIndifferentAccess.new body
 
   rescue JWT::DecodeError => e
